b6e0307b3d27d8a3156ee67ad6b355542fa40705
Gemma Gotch <pveggie@hotmail.com>
2021-08-22 09:38:26 +0200

    Add typescript to package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
   },
